442ef6bd3eef99a45f658e1d5e9d36aabdffe8ef
Aakash7405 <aakashk7405@gmail.com>
2024-05-29 18:44:01 +0530

    Delete SignupFolder directory

--- SignupFolder/router.js
+++
@@ -1,6 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const Signup =require('./Signup.js')
-router.route('/signup').post(Signup.signup);
-router.route('/verify').post(Signup.verify);
-module.exports=router;
